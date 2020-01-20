687f6e45ab6b1180d4e008231bb28120c53ce586
Jhooomn <jhooomn@gmail.com>
2020-01-19 20:08:23 -0500

    Unit test are workinng, time to get involved in this

--- src/main/java/com/app/api/infrastructure/rest/SubjectRest.java
+++ src/main/java/com/app/api/infrastructure/rest/SubjectRest.java
@@ -1,8 +1,12 @@
 package com.app.api.infrastructure.rest;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class SubjectRest {
 
 	private String id;
--- src/test/java/com/app/api/SubjectTestApplicationTests.java
+++ src/test/java/com/app/api/SubjectTestApplicationTests.java
@@ -1,13 +1,51 @@
 package com.app.api;
-
+import com.app.api.infrastructure.rest.SubjectRest;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class SubjectTestApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	ObjectMapper objectMapper;
+
 	@Test
-	void contextLoads() {
+	void contextLoads() throws  Exception{
+		mockMvc.perform(get("/subject")).andDo(print())
+		.andExpect(status().isOk())
+		.andExpect(content().contentType(MediaType.APPLICATION_JSON));
 	}
 
+	@Test
+	void saveSubject() throws Exception {
+			SubjectRest subjectRest = new SubjectRest();
+			subjectRest.setId("");
+			subjectRest.setCode("ASD752");
+			subjectRest.setCredits(3);
+			subjectRest.setName("MATH");
+
+			String json = objectMapper.writeValueAsString(subjectRest);
+
+			mockMvc.perform(post("/subject")
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(json)).andDo(print()).andExpect(status().isOk());
+		}
 }
